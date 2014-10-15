4dbd83a2a3762000f7909638ee114f2ac0839ac2
NadnerbD <brendandorr@gmail.com>
2014-10-15 17:24:52 -0400

    First successful sync download

--- sync-download.js
+++ sync-download.js
@@ -20,7 +20,7 @@ function queueDownload(source, target) {
 
 Components.utils.import("resource://gre/modules/osfile.jsm");
 
-function syncDirectories(files, dest, source, deleteFromSource) {
+function syncDirectories(files, dest, source, deleteFromSource, keepOld, log) {
 	// moves all <files> to <dest>, from <source> if available, downloading from <file.url> if not
 	// also, if <dest> is older than <file.modified>, redownload
 	Task.spawn(function () {
@@ -41,25 +41,44 @@ function syncDirectories(files, dest, source, deleteFromSource) {
 			}else{
 				var sourceInfo = null;
 			}
-			//console.log({name: file.name, dest: destInfo != null, source: sourceInfo != null});
+			//log({name: file.name, dest: destInfo != null, source: sourceInfo != null});
 			if(sourceInfo == null && destInfo == null) {
 				// we must download the file
-				console.log("File '" + file.name + "' not present. Downloading.");
+				log("File '" + file.name + "' not present. Downloading.");
 				queueDownload(file.url, destPath);
 			}else if(sourceInfo != null && destInfo == null) {
 				// we must move the file
-				console.log("File '" + file.name + "' in source. Moving.");
+				log("File '" + file.name + "' in source. Moving.");
 				OS.File.move(sourcePath, destPath).then(null, Components.utils.reportError);
 			}else if(sourceInfo != null && destInfo != null && deleteFromSource) {
 				// source file is a duplicate, let's delete it
-				console.log("Duplicate of '" + file.name + "' in source directory. Deleting.");
+				log("Duplicate of '" + file.name + "' in source directory. Deleting.");
 				OS.File.remove(sourcePath).then(null, Components.utils.reportError);
 			}
 			if(destInfo != null && destInfo.lastModificationDate < file.modified) {
 				// file is stale, redownload
-				console.log("File '" + file.name + "' has been updated. Downloading.");
-				queueDownload(file.url, destPath);
+				if(keepOld) {
+					log("File '" + file.name + "' has been updated, and keepOld is set. Moving old file.");
+					Task.spawn(function() {
+						yield OS.File.makeDir(OS.Path.join(dest, "old"), {ignoreExisting: true});
+						var num = 2;
+						var movePath = OS.Path.join(dest, "old", file.name);
+						while(yield OS.File.exists(movePath)) {
+							var movePath = OS.Path.join(dest, "old", file.name + "." + num);
+							num++;
+						}
+						yield OS.File.move(destPath, movePath);
+						log("Updated file '" + file.name + "' has been moved. Downloading.");
+						queueDownload(file.url, destPath);
+					}).then(null, Components.utils.reportError);
+				}else{
+					log("File '" + file.name + "' has been updated. Downloading.");
+					queueDownload(file.url, destPath);
+				}
 			}
 		}
+		log("Sync Complete");
 	}).then(null, Components.utils.reportError);
 }
+
+var EXPORTED_SYMBOLS = ["syncDirectories"];
