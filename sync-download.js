a47ddf6b568be7e2f4236ddbe45afa123cc6ccd8
NadnerbD <brendandorr@gmail.com>
2014-10-16 14:01:10 -0400

    Changed sync-download to behave synchronously so we can tell when it ends

--- sync-download.js
+++ sync-download.js
@@ -1,30 +1,23 @@
 Components.utils.import("resource://gre/modules/Downloads.jsm");
-Components.utils.import("resource://gre/modules/Task.jsm");
-
-function queueDownload(source, target) {
-	Task.spawn(function () {
-		let list = yield Downloads.getList(Downloads.ALL);
-		let download = yield Downloads.createDownload({
-			source: source,
-			target: target
-		});
-		list.add(download);
-		download.start();
-	}).then(null, Components.utils.reportError);
-}
-
-/* Example usage */
-/* queueDownload("https://mdn.mozillademos.org/files/6265/debug.png", "C:\\Users\\Default\\Desktop\\example-download.png"); */
-
-/* Now for the synchronization */
-
 Components.utils.import("resource://gre/modules/osfile.jsm");
+Components.utils.import("resource://gre/modules/Task.jsm");
 
 function syncDirectories(files, dest, source, deleteFromSource, keepOld, log) {
 	// moves all <files> to <dest>, from <source> if available, downloading from <file.url> if not
 	// also, if <dest> is older than <file.modified>, redownload
 	Task.spawn(function () {
 		for(var file of files) {
+			var list = yield Downloads.getList(Downloads.ALL);
+			function queueDownload(source, target) {
+				return Task.spawn(function () {
+					let download = yield Downloads.createDownload({
+						source: source,
+						target: target
+					});
+					list.add(download);
+					download.start();
+				});
+			}
 			var destPath = OS.Path.join(dest, file.name);
 			var sourcePath = OS.Path.join(source, file.name);
 			try {
@@ -45,40 +38,40 @@ function syncDirectories(files, dest, source, deleteFromSource, keepOld, log) {
 			if(sourceInfo == null && destInfo == null) {
 				// we must download the file
 				log("File '" + file.name + "' not present. Downloading.");
-				queueDownload(file.url, destPath);
+				yield queueDownload(file.url, destPath);
 			}else if(sourceInfo != null && destInfo == null) {
 				// we must move the file
 				log("File '" + file.name + "' in source. Moving.");
-				OS.File.move(sourcePath, destPath).then(null, Components.utils.reportError);
+				yield OS.File.move(sourcePath, destPath);
 			}else if(sourceInfo != null && destInfo != null && deleteFromSource) {
 				// source file is a duplicate, let's delete it
 				log("Duplicate of '" + file.name + "' in source directory. Deleting.");
-				OS.File.remove(sourcePath).then(null, Components.utils.reportError);
+				yield OS.File.remove(sourcePath);
 			}
 			if(destInfo != null && destInfo.lastModificationDate < file.modified) {
 				// file is stale, redownload
 				if(keepOld) {
 					log("File '" + file.name + "' has been updated, and keepOld is set. Moving old file.");
-					Task.spawn(function() {
-						yield OS.File.makeDir(OS.Path.join(dest, "old"), {ignoreExisting: true});
-						var num = 2;
-						var movePath = OS.Path.join(dest, "old", file.name);
-						while(yield OS.File.exists(movePath)) {
-							var movePath = OS.Path.join(dest, "old", file.name + "." + num);
-							num++;
-						}
-						yield OS.File.move(destPath, movePath);
-						log("Updated file '" + file.name + "' has been moved. Downloading.");
-						queueDownload(file.url, destPath);
-					}).then(null, Components.utils.reportError);
+					yield OS.File.makeDir(OS.Path.join(dest, "old"), {ignoreExisting: true});
+					var num = 2;
+					var movePath = OS.Path.join(dest, "old", file.name);
+					while(yield OS.File.exists(movePath)) {
+						var movePath = OS.Path.join(dest, "old", file.name + "." + num);
+						num++;
+					}
+					yield OS.File.move(destPath, movePath);
+					log("Updated file '" + file.name + "' has been moved. Downloading.");
+					yield queueDownload(file.url, destPath);
 				}else{
 					log("File '" + file.name + "' has been updated. Downloading.");
-					queueDownload(file.url, destPath);
+					yield queueDownload(file.url, destPath);
 				}
 			}
 		}
 		log("Sync Complete");
-	}).then(null, Components.utils.reportError);
+	}).then(null, function(error) {
+		log(error);
+	});
 }
 
 var EXPORTED_SYMBOLS = ["syncDirectories"];
