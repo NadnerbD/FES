ba7868dde019ba35b53528e847d82f6b1e6a7a6b
NadnerbD <brendandorr@gmail.com>
2017-01-19 12:15:51 -0500

    Added new character tags

--- characters/getchars.py
+++ characters/getchars.py
@@ -1,10 +1,21 @@
 import json
-import wget
+import urllib2
 import os
+
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
 emoteList = json.load(open("characters.json", "r"))
 for emote in emoteList:
-	fileName = emote[1].rsplit("/", 1)[1]
+	fileName = urllib2.unquote(emote[1].rsplit("/", 1)[1])
 	if not os.path.isfile(fileName):
 		print "Downloading: %s" % fileName
-		wget.download(emote[1])
+		req = urllib2.Request(emote[1], headers=hdr)
+		fp = urllib2.urlopen(req)
+		op = open(fileName, 'wb')
+		op.write(fp.read())
+		op.close()
